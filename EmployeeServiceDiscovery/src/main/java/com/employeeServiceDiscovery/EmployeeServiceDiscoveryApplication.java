f9fa2ab177c928d0d338f853773af760dc0cb345
kkm431 <ketanmishra1998@gmail.com>
2025-03-04 17:38:23 -0500

    Update for Eureka Server

---
+++ EmployeeServiceDiscovery/src/main/java/com/employeeServiceDiscovery/EmployeeServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.employeeServiceDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EmployeeServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ EmployeeServiceDiscovery/src/test/java/com/employeeServiceDiscovery/EmployeeServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
